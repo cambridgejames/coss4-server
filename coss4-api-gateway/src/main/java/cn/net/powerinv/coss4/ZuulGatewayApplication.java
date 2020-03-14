76a280fe1d4db73d7a24b3585aa4edf225180365
powerinv <cambridge_james@foxmail.com>
2020-03-14 22:31:00 +0800

    创建了基础的服务发现和网关模块

--- coss4-api-gateway/src/main/java/cn/net/powerinv/coss4/ZuulGatewayApplication.java
+++ coss4-api-gateway/src/main/java/cn/net/powerinv/coss4/ZuulGatewayApplication.java
@@ -1,4 +1,18 @@
 package cn.net.powerinv.coss4;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关应用程序
+ * @author PengJQ
+ * @date 2020-03-14
+ */
+@EnableZuulProxy
+@SpringCloudApplication
 public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
 }
