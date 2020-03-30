41e0768aad2002bf966895dd4a1c63bb912de5be
powerinv <cambridge_james@foxmail.com>
2020-03-30 21:03:46 +0800

    创建了公共实体类模块和验证码服务模块 修改了必要的项目自述文件

---
+++ coss4-services/verification-code/src/main/java/cn/net/powerinv/coss4/VerificationCodeApplication.java
@@ -0,0 +1,11 @@
+package cn.net.powerinv.coss4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationCodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VerificationCodeApplication.class, args);
+    }
+}
