9ffa59ed67b41e698030f8ca3db901244cbbd34b
powerinv <cambridge_james@foxmail.com>
2020-05-30 14:31:43 +0800

    竞赛管理模块 - 优化查询结果

--- coss4-services/competition-management/src/main/java/cn/net/powerinv/coss4/competition/base/util/CompTransformUtil.java
+++ coss4-services/competition-management/src/main/java/cn/net/powerinv/coss4/competition/base/util/CompTransformUtil.java
@@ -21,12 +21,27 @@ public class CompTransformUtil {
      * @param comp 数据库查询结果
      * @return 转换成的VO对象
      */
+    public static CompVO entity2Vo(CompVO comp) {
+        if (comp == null) {
+            return null;
+        }
+        // JSONObject detail = JSON.parseObject(comp.getDetailedInformation());
+        return new CompVO(comp.getId(), comp.getCompName(), comp.getStartTime(), comp.getUserId(), comp.getNickname(),
+                comp.getImageUrl(), comp.getEndingSign(), comp.getEnterSign(), comp.getCompState(),
+                comp.getDetailedInformation(), comp.getCreateTime(), comp.getUpdateTime());
+    }
+
+    /**
+     * Comp实体类转VO
+     * @param comp 数据库查询结果
+     * @return 转换成的VO对象
+     */
     public static CompVO entity2Vo(Comp comp) {
         if (comp == null) {
             return null;
         }
         // JSONObject detail = JSON.parseObject(comp.getDetailedInformation());
-        return new CompVO(comp.getId(), comp.getCompName(), comp.getStartTime(), comp.getUserId(),
+        return new CompVO(comp.getId(), comp.getCompName(), comp.getStartTime(), comp.getUserId(), null,
                 comp.getImageUrl(), comp.getEndingSign(), comp.getEnterSign(), comp.getCompState(),
                 comp.getDetailedInformation(), comp.getCreateTime(), comp.getUpdateTime());
     }
@@ -36,10 +51,10 @@ public class CompTransformUtil {
      * @param compList 数据库查询结果
      * @return 转换成的VO对象列表
      */
-    public static List<CompVO> entity2VoList(List<Comp> compList) {
+    public static List<CompVO> entity2VoList(List<CompVO> compList) {
         List<CompVO> compVOList = new ArrayList<>();
         if (compList != null && compList.size() != 0) {
-            for (Comp comp : compList) {
+            for (CompVO comp : compList) {
                 compVOList.add(entity2Vo(comp));
             }
         }
