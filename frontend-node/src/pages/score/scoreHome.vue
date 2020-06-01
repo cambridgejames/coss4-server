c93cb91d83cfdd1ee174bb9386db322eebe73903
powerinv <cambridge_james@foxmail.com>
2020-06-01 15:49:14 +0800

    修改Eureka服务相关配置，修复一断网就会瞎写日志的BUG

--- frontend-node/src/pages/score/scoreHome.vue
+++ frontend-node/src/pages/score/scoreHome.vue
@@ -12,11 +12,26 @@
                 <el-button type="primary" @click="startScoring">进入评分</el-button>
                 <el-button @click="logoutImpl">退出</el-button>
             </div>
-            <el-card style="margin-top: 40px;">{{competitionInfo}}</el-card>
+            <el-card class="tips">
+                <p>{{competitionInfo}}</p>
+                <p>若要测试系统网络连接，可以<el-link type="primary" @click="networkTest">点击此处</el-link>。</p>
+            </el-card>
         </div>
         <div v-else>
             scoring
         </div>
+        <el-dialog title="系统测试" :visible.sync="dialogVisible" :close-on-click-modal="false" :close-on-press-escape="false" :show-close="false"
+                   width="100%" style="max-width: 600px; margin-left: auto; margin-right: auto;">
+            <el-timeline :reverse="reverse">
+                <el-timeline-item v-for="(activity, index) in activities" :key="index" :type="activity.type" size="large" :icon="activity.icon">
+                    {{activity.content}}
+                </el-timeline-item>
+            </el-timeline>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false" :disabled="true">取 消</el-button>
+                <el-button type="primary" @click="dialogVisible = false" :disabled="dialogConfirmDisabled">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -34,7 +49,11 @@
                 competitionInfo: {
                     tagMode: {}
                 },
-                isScoring: false
+                isScoring: false,
+                dialogVisible: false,
+                dialogConfirmDisabled: true,
+                reverse: false,
+                activities: []
             }
         },
         mounted() {
@@ -56,6 +75,42 @@
             logoutImpl() {
                 this.logout(this.successMessage, this.errorMessage);
                 this.$router.push({path: '/login'});
+            },
+            networkTest() {
+                let that = this;
+                that.activities = [
+                    {content: '检查系统环境'}, {content: '测试网络连通性'}, {content: '模拟成绩提交'}, {content: '成功'}
+                ];
+                that.dialogConfirmDisabled = true;
+                that.dialogVisible = true;
+                that.activities[0].icon = 'el-icon-loading';
+                that.activities[0].type = 'primary';
+                that.$set(that.activities, 0, that.activities[0]);
+                setTimeout(function() {
+                    that.activities[0].icon = 'el-icon-check';
+                    that.activities[0].type = 'success';
+                    that.$set(that.activities, 0, that.activities[0]);
+                    that.activities[1].icon = 'el-icon-loading';
+                    that.activities[1].type = 'primary';
+                    that.$set(that.activities, 1, that.activities[1]);
+                    setTimeout(function() {
+                        that.activities[1].icon = 'el-icon-check';
+                        that.activities[1].type = 'success';
+                        that.$set(that.activities, 1, that.activities[1]);
+                        that.activities[2].icon = 'el-icon-loading';
+                        that.activities[2].type = 'primary';
+                        that.$set(that.activities, 2, that.activities[2]);
+                        setTimeout(function() {
+                            that.activities[2].icon = 'el-icon-check';
+                            that.activities[2].type = 'success';
+                            that.$set(that.activities, 2, that.activities[2]);
+                            that.activities[3].icon = 'el-icon-check';
+                            that.activities[3].type = 'success';
+                            that.$set(that.activities, 3, that.activities[3]);
+                            that.dialogConfirmDisabled = false;
+                        }, 3000);
+                    }, 3000);
+                }, 3000);
             }
         }
     }
@@ -83,6 +138,12 @@
                 &:last-child {
                     margin-right: 0;
                 }
+            }
+        }
+        .tips {
+            margin-top: 40px;
+            p {
+                margin-bottom: 10px;
             }
         }
     }
